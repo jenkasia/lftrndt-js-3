14059d1214c4f45900f9479d105423fd90653644
Yevhenii Kasian <yevgenii.kasian@gmail.com>
2023-04-19 18:05:44 -0500

    task2

--- main.js
+++ main.js
@@ -20,6 +20,7 @@ async function getWeatherData(startDate, endDate) {
     const data = await response.json();
 
     if (data.daily.time.length > 0) {
+      createXMLButton(data);
       const table = generateTable(data.daily);
       document.getElementById("table-container").innerHTML = table;
     } else {
@@ -54,13 +55,6 @@ function generateTable(data) {
   return table;
 }
 
-document.getElementById("start-tracking").addEventListener("click", () => {
-  const week = document.getElementById("week-select").value;
-  const startDate = getStartDateOfWeek(week);
-  const endDate = getEndDateOfWeek(week);
-  getWeatherData(startDate, endDate);
-});
-
 function getStartDateOfWeek(week) {
   const currentDate = new Date();
   const currentYear = currentDate.getFullYear();
@@ -93,3 +87,77 @@ function formatDate(date) {
   const day = String(date.getDate()).padStart(2, "0");
   return `${year}-${month}-${day}`;
 }
+
+function generateXML(data) {
+  const { time, temperature_2m_min, temperature_2m_max } = data;
+  let xml = '<?xml version="1.0" encoding="UTF-8"?>\n<temperatures>\n';
+  time.forEach((date, index) => {
+    const dayOfWeek = new Date(date).toLocaleDateString("en-US", {
+      weekday: "long",
+    });
+    const formattedDate = new Date(date).toLocaleDateString("en-US");
+    const minTemp = temperature_2m_min[index];
+    const maxTemp = temperature_2m_max[index];
+    xml += `\t<temperature>\n\t\t<day>${dayOfWeek}</day>\n\t\t<date>\n\t\t\t<dateValue>${formattedDate}</dateValue>\n\t\t\t<dateFormat>YYYY-MM-DD</dateFormat>\n\t\t</date>\n\t\t<min>${minTemp}</min>\n\t\t<max>${maxTemp}</max>\n\t</temperature>\n`;
+  });
+  xml += "</temperatures>";
+  downloadXML(xml);
+}
+
+function downloadXML(xml) {
+  const filename = "temperature_data.xml";
+  const blob = new Blob([xml], { type: "text/xml;charset=utf-8" });
+  if (navigator.msSaveBlob) {
+    navigator.msSaveBlob(blob, filename);
+  } else {
+    const link = document.createElement("a");
+    if (link.download !== undefined) {
+      const url = URL.createObjectURL(blob);
+      link.setAttribute("href", url);
+      link.setAttribute("download", filename);
+      link.style.visibility = "hidden";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
+  }
+}
+
+function handleXMLButtonClick(data) {
+  generateXML(data.daily);
+}
+
+function createXMLButton(data) {
+  let xmlButton = document.getElementById("generate-xml");
+  if (!xmlButton) {
+    xmlButton =
+      document.getElementById("generate-xml") ||
+      document.createElement("button");
+    xmlButton.id = "generate-xml";
+    xmlButton.textContent = "Generate XML";
+    document.getElementById("button-container").appendChild(xmlButton);
+  } else {
+    xmlButton.removeEventListener("click", handleXMLButtonClick);
+  }
+
+  xmlButton.addEventListener("click", handleXMLButtonClick.bind(null, data));
+}
+
+function main() {
+  document.getElementById("start-tracking").addEventListener("click", () => {
+    const week = document.getElementById("week-select").value;
+    const startDate = getStartDateOfWeek(week);
+    const endDate = getEndDateOfWeek(week);
+    getWeatherData(startDate, endDate);
+  });
+
+  document.getElementById("week-select").addEventListener("change", (event) => {
+    const xmlButton = document.getElementById("generate-xml");
+    document.getElementById("table-container").innerHTML = "";
+    if (xmlButton) {
+      xmlButton.remove();
+    }
+  });
+}
+
+main();
