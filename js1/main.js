da2f4eafa8fc33af2e2c35012cb2493116fa57df
Yevhenii Kasian <yevgenii.kasian@gmail.com>
2023-04-19 16:00:15 -0500

    Task 1

---
+++ js1/main.js
@@ -0,0 +1,95 @@
+const BASE_URL = "https://api.open-meteo.com/v1/forecast";
+const LATITUDE = 40.71;
+const LONGITUDE = -74.01;
+const TIMEZONE = "America/New_York";
+const DAILY = "temperature_2m_max,temperature_2m_min";
+
+async function getWeatherData(startDate, endDate) {
+  const params = new URLSearchParams({
+    latitude: LATITUDE,
+    longitude: LONGITUDE,
+    timezone: TIMEZONE,
+    daily: DAILY,
+    start_date: startDate,
+    end_date: endDate,
+  });
+
+  try {
+    const response = await fetch(`${BASE_URL}?${params.toString()}`);
+
+    const data = await response.json();
+
+    if (data.daily.time.length > 0) {
+      const table = generateTable(data.daily);
+      document.getElementById("table-container").innerHTML = table;
+    } else {
+      document.getElementById("table-container").innerHTML =
+        "No data available for the selected week.";
+    }
+  } catch (error) {
+    document.getElementById(
+      "table-container"
+    ).innerHTML = `<div class='error'><p>An error occurred.</p></div>`;
+
+    console.error(error);
+  }
+}
+
+function generateTable(data) {
+  let table =
+    "<table>\n<thead>\n<tr>\n<th>Day</th>\n<th>Date</th>\n<th>Min Temperature</th>\n<th>Max Temperature</th>\n</tr>\n</thead>\n<tbody>\n";
+  const { time, temperature_2m_min, temperature_2m_max } = data;
+
+  time.forEach((date, index) => {
+    const dayOfWeek = new Date(date).toLocaleDateString("en-US", {
+      weekday: "long",
+    });
+    const formattedDate = new Date(date).toLocaleDateString("en-US");
+    const minTemp = temperature_2m_min[index];
+    const maxTemp = temperature_2m_max[index];
+    table += `<tr>\n<td>${dayOfWeek}</td>\n<td>${formattedDate}</td>\n<td>${minTemp} &deg;C</td>\n<td>${maxTemp} &deg;C</td>\n</tr>\n`;
+  });
+
+  table += "</tbody>\n</table>";
+  return table;
+}
+
+document.getElementById("start-tracking").addEventListener("click", () => {
+  const week = document.getElementById("week-select").value;
+  const startDate = getStartDateOfWeek(week);
+  const endDate = getEndDateOfWeek(week);
+  getWeatherData(startDate, endDate);
+});
+
+function getStartDateOfWeek(week) {
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  const currentMonth = currentDate.getMonth();
+  const firstDayOfMonth = new Date(currentYear, currentMonth, 1);
+  const daysSinceMonday = (7 + firstDayOfMonth.getDay() - 1) % 7;
+
+  const startDate = new Date(
+    currentYear,
+    currentMonth,
+    1 + (week - 1) * 7 - daysSinceMonday
+  );
+  return formatDate(startDate);
+}
+
+function getEndDateOfWeek(week) {
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  const currentMonth = currentDate.getMonth();
+  const firstDayOfMonth = new Date(currentYear, currentMonth, 1);
+  const daysOffset =
+    firstDayOfMonth.getDay() === 0 ? 6 : firstDayOfMonth.getDay() - 1;
+  const endDate = new Date(currentYear, currentMonth, week * 7 - daysOffset);
+  return endDate.toISOString().slice(0, 10);
+}
+
+function formatDate(date) {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  return `${year}-${month}-${day}`;
+}
